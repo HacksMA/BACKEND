b0dc4408eab8f4783f1f9cfc16f0d08c4994f0cb
HacksMA <33467178+HacksMA@users.noreply.github.com>
2018-04-15 23:26:30 +0300

    bigchange

--- index/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-
-from index import views
-
-urlpatterns = [
-    path('2/', views.indexRender)
-]
--- index/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def indexRender(request):
-    returnData = dict()
-    RTlist = {a: a ** 2 for a in range(7)}
-    returnData.update(a=RTlist)
-    return render(request, 'mainpage/1.html', returnData)
-
